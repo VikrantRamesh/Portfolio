4b1e6f0257283894f19b18870c91e67eb3154b5a
VikrantRamesh <94131187+VikrantRamesh@users.noreply.github.com>
2024-04-11 16:22:33 +0530

    Final Deign implementations

--- javascript/main.js
+++ javascript/main.js
@@ -21,17 +21,17 @@ window.addEventListener("scroll", function () {
   
   // Ensure translateX is within 0 to 100
   translateX = Math.min(Math.max(translateX, 0), 100);
-  console.log((scrollPos / maxScroll)*5/2.37)
-  if(translateX < 50){
+  
+  if(translateX < 32){
       // Move the object from right to left
       $('.about-me-image-container').css({
-          'transform': 'translateX(' +Math.max(45 - translateX, 0)+ 'rem)',
+          'transform': 'translateX(' +Math.max(33 - translateX, 0)+ 'rem)',
           'opacity': Math.min((scrollPos / maxScroll)*5, 1)
       });
   }else{
       $('.about-me-image-container').css({
-          'transform': 'translateX(' +Math.min(translateX-50, 50)+ 'rem)',
-          'opacity': ((scrollPos / maxScroll)*5/2.37)
+          'transform': 'translateX(' +Math.min(translateX-32, 32)+ 'rem)',
+          'opacity': 1/((scrollPos / maxScroll)*5/1.56)
       });
   }
 }, false);
@@ -52,29 +52,28 @@ window.addEventListener("scroll", function () {
 
   // Ensure translateY is within 0 to 100
   translateY = Math.min(Math.max(translateY, 0)*10, 1000);
-  
 
   if (window.matchMedia("(max-width: 600px)").matches) {
-    if (translateY < 500) {
+    if (translateY < 350) {
       $('.about-me-text').css({
-        'transform': 'scale(' + Math.min(translateY / 500, 1) + ') ',
+        'transform': 'scale(' + Math.min(translateY / 350, 1) + ') ',
         'opacity': Math.min((scrollPos / maxScroll) * 5, 0.9)
       });
     } else {
       $('.about-me-text').css({
-        'transform': 'translateX(' + Math.max(-1 * (translateY - 500), -1000) + 'px) ',
+        'transform': 'translateX(' + Math.max(-1 * (translateY - 350), -1000) + 'px) ',
         'opacity': Math.min(3.4 - (scrollPos / maxScroll) * 5, 0.9)
       });
     }
   } else {
-    if (translateY < 460) {
+    if (translateY < 320) {
       $('.about-me-text').css({
-        'transform': 'scale(' + Math.min(translateY / 460, 1) + ') ',
+        'transform': 'scale(' + Math.min(translateY / 320, 1) + ') ',
         'opacity': Math.min((scrollPos / maxScroll) * 5, 0.9)
       });
     } else {
       $('.about-me-text').css({
-        'transform': 'translateX(' + Math.max(-1 * (translateY - 460), -1000) + 'px) ',
+        'transform': 'translateX(' + Math.max(-1 * (translateY - 320), -1000) + 'px) ',
         'opacity': Math.min(3.4 - (scrollPos / maxScroll) * 5, 0.9)
       });
     }
@@ -151,6 +150,7 @@ if(addingWord){
   }
 }
 }
+
 
 
 //scrolling text
@@ -230,4 +230,115 @@ ScrollTrigger.create({
   },
 });
 
+//capsule
 
+const initialTranslateY = gsap.utils.toArray('.scroll-smooth-effects__circle.layer1').map(circle => gsap.getProperty(circle, 'y'));
+
+gsap.utils.toArray('.scroll-smooth-effects__circle').forEach((circle, index) => {
+  gsap.to(circle, {
+    scrollTrigger: {
+      trigger: circle,
+      start: 'top bottom', // Trigger animation when the top of the element hits the bottom of the viewport
+      end: '+=400%',
+      scrub: true,
+      toggleActions: 'play none none reverse',
+      onUpdate: self => {
+        const progress = self.progress;
+        const translateY = initialTranslateY[index] + (-2000 * progress); // Adjust the factor (-50) based on the desired translation range
+        gsap.set(circle, { y: translateY });
+      }
+    },
+    ease: 'power2.out'
+  });
+});
+
+// window.addEventListener('scroll', function() {
+//   console.log(window.scrollY);
+// });
+// const initialTranslateY2 = gsap.utils.toArray('.scroll-smooth-effects__circle.layer2').map(circle => gsap.getProperty(circle, 'y'));
+// gsap.utils.toArray('.scroll-smooth-effects__circle').forEach((circle, index) => {
+//   gsap.to(circle, {
+//     scrollTrigger: {
+//       trigger: circle,
+//       start: 'top bottom', // Trigger animation when the top of the element hits the bottom of the viewport
+//       end: '+=400%',
+//       scrub: true,
+//       toggleActions: 'play none none reverse',
+//       onUpdate: self => {
+//         const progress = self.progress;
+//         const translateY = initialTranslateY2[index] + (-2000 * progress); // Adjust the factor (-50) based on the desired translation range
+//         gsap.set(circle, { y: translateY });
+//       }
+//     },
+//     height: '50vh',
+//     ease: 'power2.out'
+//   });
+// });
+
+// ScrollTrigger for height adjustment
+const initialHeight = gsap.utils.toArray('.scroll-smooth-effects__circle').map(circle => gsap.getProperty(circle, 'height'));
+gsap.utils.toArray('.scroll-smooth-effects__circle').forEach((circle, index) => {
+  gsap.to(circle, {
+    scrollTrigger: {
+      trigger: circle,
+      start: () => window.scrollY >= 3000? 'top top' : '+=10', // Start animation when window.scrollY is equal to 3000
+      end: '+=400%', // Extend the height adjustment by 400% of the viewport height
+      scrub: true,
+      toggleActions: 'play none none reverse',
+      onUpdate: self => {
+        const progress = self.progress;
+        const newHeight = initialHeight[index] * (1 + progress * 20); // Adjust the factor (3) based on the desired height increase range
+        gsap.set(circle, { height: newHeight });
+      }
+    },
+    ease: 'power2.out'
+  });
+});
+// const lastCircle = document.querySelector('.scroll-smooth-effects__circle:last-of-type');
+
+// // Pin the .info section and #main1 section until the lastCircle exits the viewport
+// gsap.to('.info, #main1', {
+//   scrollTrigger: {
+//     trigger: '#curtain', // Trigger on the #curtain element
+//     start: 'top top',
+//     endTrigger: lastCircle, // End trigger on the last .scroll-smooth-effects__circle element
+//     end: 'bottom bottom', // End pinning when the bottom of the last circle element exits the viewport
+//     pin: true,
+//     pinSpacing: false,
+//     scrub: true,
+//     toggleActions: 'play none none reverse'
+//   }
+// });
+
+// ScrollTrigger.create({
+//   trigger: ".info",
+//   pin: true,
+//   start: "center center",
+//   end: "+=200%",
+//   markers: true
+// });
+
+
+console.clear();
+    document.addEventListener("DOMContentLoaded", function (event) {
+      const timeline = gsap.timeline({
+        defaults: {
+          ease: "none",
+        },
+        scrollTrigger: {
+          trigger: "#container2",
+          pin: true,
+          start: "top",
+          end: "bottom",
+          scrub: 1,
+          markers: false,
+        },
+      });
+const circles = gsap.utils.toArray('section.circle');
+circles.forEach(circle => {      
+      timeline.from(circle, {
+        clipPath: "circle(0% at 50% 50%)",
+        stagger: 1,
+      });
+  })
+    });
